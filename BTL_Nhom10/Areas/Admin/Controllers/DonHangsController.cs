2eb140f7657cb13957851e25f87d56f4f4583e4b
vietthai2099@gmail.com <vietthai2099@gmail.com>
2020-12-23 10:36:31 +0700

    change 2

---
+++ BTL_Nhom10/Areas/Admin/Controllers/DonHangsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BTL_Nhom10.Areas.Admin.Models;
+
+namespace BTL_Nhom10.Areas.Admin.Controllers
+{
+    public class DonHangsController : Controller
+    {
+        private Models.Database db = new Models.Database();
+
+        // GET: Admin/DonHangs
+        public ActionResult Index()
+        {
+            return View(db.DonHangs.ToList());
+        }
+
+        // GET: Admin/DonHangs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // GET: Admin/DonHangs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/DonHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDH,NgayDat,NguoiDat,DiaChi,HinhThucTT,TongTien")] DonHang donHang)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.DonHangs.Add(donHang);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Lỗi nhập dữ liệu!" + ex.Message;
+                return View(donHang);
+            }
+        }
+
+        // GET: Admin/DonHangs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // POST: Admin/DonHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaDH,NgayDat,NguoiDat,DiaChi,HinhThucTT,TongTien")] DonHang donHang)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(donHang).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Lỗi nhập dữ liệu!" + ex.Message;
+                return View(donHang);
+            }
+        }
+        // GET: Admin/DonHangs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonHang donHang = db.DonHangs.Find(id);
+            if (donHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donHang);
+        }
+
+        // POST: Admin/DonHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            DonHang donHang = db.DonHangs.Find(id);
+            try
+            {
+                db.DonHangs.Remove(donHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Error = "Không xóa được bản ghi này!" + ex.Message;
+                return View("Delete", donHang);
+            }
+            }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- BTL_Nhom10/Areas/Admin/Controllers/HomeController.cs
+++ BTL_Nhom10/Areas/Admin/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BTL_Nhom10.Areas.Admin.Models;
 namespace BTL_Nhom10.Areas.Admin.Controllers
 {
     public class HomeController : Controller
@@ -11,7 +11,14 @@ namespace BTL_Nhom10.Areas.Admin.Controllers
         // GET: Admin/Home
         public ActionResult Index()
         {
-            return View();
+            if(Session["TaiKhoan1"] !=null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
 
         public ActionResult Category()
@@ -43,10 +50,39 @@ namespace BTL_Nhom10.Areas.Admin.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        Database db = new Database();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string TaiKhoan1, string MatKhau)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = db.TaiKhoans.Where(u => u.TaiKhoan1.Equals(TaiKhoan1) && u.MatKhau.Equals(MatKhau)).ToList();
+                if(user.Count() > 0)
+                {
+                    Session["TaiKhoan1"] = user.FirstOrDefault().TaiKhoan1;
+                    Session["MatKhau"] = user.FirstOrDefault().MatKhau;
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    ViewBag.Error = "Sai tài khoản hoặc mật khẩu!";
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
         }
     }
 }
